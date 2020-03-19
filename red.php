26501182788d28e0b57dd735db77ca0e3fe094dd
iosf45 <62310366+iosf45@users.noreply.github.com>
2020-03-18 22:56:51 -0400

    Update red.php

--- red.php
+++ red.php
@@ -1,3 +1,11 @@
-<?php 
-   header('Location: https://atom.io/'); 
-?>
+<html> 
+    <head> 
+        <meta charset="utf-5"/> 
+        <script text="javascript"> 
+            setTimeout(redirigir,5000); 
+            function redirigir(){ 
+                window.location="http://www.solvetic.com"; 
+            } 
+        </script> 
+    </head> 
+   </html>
